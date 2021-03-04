463532223477d5f75aae580390bf1c12dc96411b
Luigi <aprendizferrari1997@gmail.com>
2021-03-04 14:48:31 -0300

    tests

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lenzo-pe <lenzo-pe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 23:30:57 by lenzo-pe          #+#    #+#             */
+/*   Updated: 2021/03/04 14:36:22 by lenzo-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <sys/resource.h>
+# include <fcntl.h>
+
+# define FT_EOL			1
+# define FT_EOF			0
+# define FT_ERROR		-1
+# define FT_LIMIT_FD	RLIMIT_NOFILE
+
+int		get_next_line(int fd, char **line);
+void	*ft_memcpy(void *dest, const void *src, size_t num);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strclen(const char *str, const char chr);
+void	ft_strdel(char **str);
+
+#endif
